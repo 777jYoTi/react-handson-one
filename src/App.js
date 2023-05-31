afe811aa6e16d636d5dd2382ca47337b6cf37f7e
Jyoti <jyotiranjanbaliarsingh777@gmai.com>
2023-05-31 10:47:07 +0530

    h1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import ClassComponent from './ClassComponent';
+import FunctionComponent from './FunctionComponent';
+import { Component } from 'react';
+
+
+
+
+class Jyoti extends Component{
+
+constructor(){
+    super();
+    this.state = {
+        FunctionClick: false,
+        ClassClick: false
+    }
 }
 
-export default App;
+  render(){
+    return(
+        <div ClassName="App ">
+          <span className='love'>
+          <h1>Styling Using Function And Class Component</h1>
+          <h1>Styling Using Function And Class Component</h1>
+          </span>
+           
+            
+
+            <center className='all'>
+            <button onClick={()=>this.setState({FunctionClick:!this.state.FunctionClick})}> To See Styling In Functin Component</button>
+            <button onClick={()=>this.setState({ ClassClick:!this.state.ClassClick})}>To See Styling In Class Component</button>
+            </center>
+            
+            
+            {this.state.FunctionClick && <FunctionComponent/>}
+            {this.state.ClassClick && <ClassComponent/>}
+
+        </div>
+    )
+  }
+}
+export default Jyoti
